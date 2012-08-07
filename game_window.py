db5a5f2992246bbcf359fb639fc8c11d70339978
Nhat-Tan Duong <vinthian@gmail.com>
2012-08-06 23:38:56 -0400

    optimized Life.next() and pygame drawing

--- game_window.py
+++ game_window.py
@@ -1,60 +1,110 @@
 import pygame, sys
 from pygame.locals import *
-
 from life import *
-
 from random import randint
 
-# Game/Display Settings
-width = 50
-height = 50
-pixel_size = 5
+class Game:
+    def __init__(self, start=True):
+        # Game/Display Settings
+        self.width = 100
+        self.height = 100
+        self.pixel_size = 5
 
-# Pygame
-pygame.init()
-pygame.display.init()
-fps_clock = pygame.time.Clock()
+        # Pygame
+        pygame.init()
+        pygame.display.init()
+        self.fps_clock = pygame.time.Clock()
+        self.running = True
+        self.paused = False
+        self.color = 0
+        self.speed = 30
 
-game_window = pygame.display.set_mode((width * pixel_size, height * pixel_size))
+        self.game_window = pygame.display.set_mode(
+            (self.width * self.pixel_size, self.height * self.pixel_size))
 
-black_color = pygame.Color(0, 0, 0)
-white_color = pygame.Color(255, 255, 255)
+        self.black_color = pygame.Color(0, 0, 0)
+        self.white_color = pygame.Color(255, 255, 255)
 
-color_values = [(x + 1) * 31 for x in range(8)]
-life_colors = [pygame.Color(x, x, x) for x in color_values]
+        color_values = [280 - ((x + 1) * 25) for x in range(8)]
+        gray_values = [55 + (x + 1) * 25 for x in range(8)]
+        self.life_colors = [pygame.Color(x, 0, 0) for x in color_values]
+        self.gray_colors = [pygame.Color(x, x, x) for x in gray_values]
 
-def draw_pixel(x, y, color):
-    pygame.draw.rect(game_window, color,
-                     (x * pixel_size, y * pixel_size,
-                      pixel_size, pixel_size))
+        # Life
+        self.life = Life(self.width, self.height)
+        self.life._gospers_glider_gun()
+        """
+        for x in range(5):
+        life._glider(randint(0, width - 3), randint(0, height - 3))
+        life._small_explorer(randint(0, width - 13), randint(0, height - 13))
+        """
 
-# Life
-life = Life(width, height)
-life._glider(10, 10)
-life._small_explorer(30, 10)
+        # Auto-start
+        if start:
+            self.main()
 
-running = True
-while running:
-    pygame.display.set_caption("Life - gen: %s - fps: %s" %
-                               (life.generation, round(fps_clock.get_fps())))
-    game_window.fill(white_color)
-    
-    for y in range(life.grid.height):
-        for x in range(life.grid.width):
-            if life.grid[x, y] == 1:
-                draw_pixel(x, y, life_colors[life.neighbor_count(x, y) - 1])
+    def draw_pixel(self, x, y, color):
+        pygame.draw.rect(self.game_window, color,
+                         (x * self.pixel_size, y * self.pixel_size,
+                          self.pixel_size, self.pixel_size))
 
-    life.next()
+    def key_helper(self):
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                self.running = False
+                self.paused = False
+            elif event.type == KEYDOWN:
+                if event.key == K_1:
+                    if self.speed - 15 > 0:
+                        self.speed -= 15
+                if event.key == K_2:
+                    self.speed += 15
+                if event.key == K_3:
+                    if self.color - 1 >= 0:
+                        self.color -= 1
+                        self.draw_update()
+                if event.key == K_4:
+                    if self.color + 1 <= 2:
+                        self.color += 1
+                        self.draw_update()
+                if event.key == K_p:
+                    self.paused = not self.paused
+                if event.key == K_ESCAPE:
+                    pygame.event.post(pygame.event.Event(QUIT))
 
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            running = False
-        elif event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
-                pygame.event.post(pygame.event.Event(QUIT))
+    def draw_update(self):
+        self.game_window.fill(self.white_color)
+        for x, y in self.life.dirty_list:
+            if self.life.grid[x, y] == 1:
+                if self.color == 0:
+                    self.draw_pixel(
+                        x, y,
+                        self.life_colors[self.life.neighbor_count(x, y) - 1])
+                elif self.color == 1:
+                    self.draw_pixel(
+                        x, y,
+                        self.gray_colors[self.life.neighbor_count(x, y) - 1])
+                else:
+                    self.draw_pixel(x, y, self.black_color)
+        pygame.display.update()
 
-    pygame.display.update()
-    fps_clock.tick(30)
+    def main(self):
+        # Main Game Loop
+        while self.running:
+            pygame.display.set_caption("gen: %s - fps: %s/%s - updates: %s" %
+                                       (self.life.generation,
+                                        round(self.fps_clock.get_fps()),
+                                        self.speed,
+                                        self.life.update_count))
+            self.draw_update()
+            self.key_helper()
 
-pygame.quit()
-sys.exit()
+            while self.paused:
+                self.key_helper()
+
+            self.life.next()
+            self.fps_clock.tick(self.speed)
+
+        # Quit
+        pygame.quit()
+        sys.exit()
--- life.py
+++ life.py
@@ -1,16 +1,35 @@
 from bit_array import BitArray2D
 
 class LifeGrid(BitArray2D):
+    def __init__(self, width, height):
+        self._alive_set = set()
+        self._dirty_set = set()
+        super().__init__(width, height)
+
     def __setitem__(self, coords, value):
         x, y = coords
-        if x == -1 or x == self._width or y == -1 or y == self._height:
-            pass
-        else:
-            super().__setitem__(coords, value)
+        if coords[0] not in range(0, self._width) or \
+                coords[1] not in range(0, self._height):
+            return
+
+        for near_x, near_y in [(x - 1 + i, y - 1 + j)
+                               for i in range(3) for j in range(3)]:
+            if near_x in range(0, self._width) and \
+                    near_y in range(0, self._height):
+                if value and coords not in self._alive_set:
+                    self._dirty_set.add((near_x, near_y))
+                if not value and coords in self._alive_set:
+                    if (near_x, near_y) in self._dirty_set:
+                        self._dirty_set.remove((near_x, near_y))
+        if value and coords not in self._alive_set:
+            self._alive_set.add(coords)
+        if not value and coords in self._alive_set:
+            self._alive_set.add(coords)
+        super().__setitem__(coords, value)
 
     def __getitem__(self, coords):
         x, y = coords
-        if x == -1 or x == self._width or y == -1 or y == self._height:
+        if x not in range(0, self._width) or y not in range(0, self._height):
             return False
         else:
             return super().__getitem__(coords)
@@ -29,28 +48,13 @@ class Life(object):
         self.grid = LifeGrid(width, height)
         self.generation = 0
 
-    def next(self):
-        new_grid = LifeGrid(self.grid.width, self.grid.height)
-        for y in range(self.grid.height):
-            for x in range(self.grid.width):
-                neighbor_count = self.neighbor_count(x, y)
-                if self.grid[x, y] == 1:
-                    # Any live cell with fewer than two live neighbours dies,
-                    #   as if caused by under-population.
-                    # Any live cell with more than three live neighbours dies,
-                    #   as if by overcrowding.
-                    if neighbor_count < 2 or neighbor_count > 3:
-                        new_grid[x, y] = 0
-                    # Any live cell with two or three live neighbours lives on to
-                    #   the next generation.
-                    if neighbor_count == 2 or neighbor_count == 3:
-                        new_grid[x, y] = 1
-                # Any dead cell with exactly three live neighbours becomes a
-                #   live cell, as if by reproduction.
-                if self.grid[x, y] == 0 and neighbor_count == 3:
-                    new_grid[x, y] = 1
-        self.grid = new_grid
-        self.generation += 1
+    @property
+    def update_count(self):
+        return len(self.grid._dirty_set)
+
+    @property
+    def dirty_list(self):
+        return self.grid._dirty_set
 
     def neighbor_count(self, x, y):
         neighbor_pos = [(x - 1, y - 1), (x - 1, y), (x - 1, y + 1),
@@ -61,13 +65,28 @@ class Life(object):
             neighbor_count += self.grid[coords]
         return neighbor_count
 
-    def draw(self):
-        for y in range(self.grid.height):
-            row = ""
-            for x in range(self.grid.width):
-                row += (str(self.neighbor_count(x, y)) if self.grid[x, y]
-                        else chr(183))
-            print(row)
+    def next(self):
+        new_grid = LifeGrid(self.grid.width, self.grid.height)
+        for coords in self.grid._alive_set.union(self.grid._dirty_set):
+            neighbor_count = self.neighbor_count(*coords)
+            if self.grid[coords] == 1:
+                # Any live cell with fewer than two live neighbours dies,
+                #   as if caused by under-population.
+                # Any live cell with more than three live neighbours dies,
+                #   as if by overcrowding.
+                if neighbor_count < 2 or neighbor_count > 3:
+                    new_grid[coords] = 0
+                # Any live cell with two or three live neighbours lives on to
+                #   the next generation.
+                if neighbor_count == 2 or neighbor_count == 3:
+                    new_grid[coords] = 1
+                # Any dead cell with exactly three live neighbours becomes a
+                #   live cell, as if by reproduction.
+            if self.grid[coords] == 0 and neighbor_count == 3:
+                new_grid[coords] = 1
+        self.grid = new_grid
+        self.generation += 1
+        return True
 
     def _glider(self, x = 0, y = 0):
         # Clear out 3x3 area
@@ -94,3 +113,41 @@ class Life(object):
         self.grid[x    , y + 2] = 1
         self.grid[x + 2, y + 2] = 1
         self.grid[x + 1, y + 3] = 1
+
+    def _gospers_glider_gun(self, x=0, y=0):
+        self.grid[x + 1, y + 5] = 1 # XX
+        self.grid[x + 2, y + 5] = 1 # XX
+        self.grid[x + 2, y + 6] = 1
+        self.grid[x + 2, y + 6] = 1
+        self.grid[x + 11, y + 5] = 1 #   XX
+        self.grid[x + 11, y + 6] = 1 #  X
+        self.grid[x + 11, y + 7] = 1 # X
+        self.grid[x + 12, y + 4] = 1 # X
+        self.grid[x + 12, y + 8] = 1 # X
+        self.grid[x + 13, y + 3] = 1 #  X
+        self.grid[x + 13, y + 9] = 1 #   XX
+        self.grid[x + 14, y + 3] = 1
+        self.grid[x + 14, y + 9] = 1
+        self.grid[x + 15, y + 6] = 1 #  X
+        self.grid[x + 16, y + 4] = 1 #   X
+        self.grid[x + 16, y + 8] = 1 # X XX
+        self.grid[x + 17, y + 5] = 1 #   X
+        self.grid[x + 17, y + 6] = 1 #  X
+        self.grid[x + 17, y + 7] = 1 #
+        self.grid[x + 18, y + 6] = 1
+        self.grid[x + 21, y + 3] = 1 #     X
+        self.grid[x + 21, y + 4] = 1 #   X X
+        self.grid[x + 21, y + 5] = 1 # XX
+        self.grid[x + 22, y + 3] = 1 # XX
+        self.grid[x + 22, y + 4] = 1 # XX
+        self.grid[x + 22, y + 5] = 1 #   X X
+        self.grid[x + 23, y + 2] = 1 #     X
+        self.grid[x + 23, y + 6] = 1
+        self.grid[x + 25, y + 1] = 1
+        self.grid[x + 25, y + 2] = 1
+        self.grid[x + 25, y + 6] = 1
+        self.grid[x + 25, y + 7] = 1
+        self.grid[x + 35, y + 4] = 1
+        self.grid[x + 35, y + 5] = 1
+        self.grid[x + 36, y + 4] = 1
+        self.grid[x + 36, y + 5] = 1
